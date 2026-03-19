6dc616cdad6da5d4c24a16b931d9a35c90cb5882
Test <adnreyslakbash@gmail.com>
2026-03-20 01:51:21 +0500

    Выпольнил заданеи

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,16 +1,16 @@
 
-let tabs = document.querySelectorAll('.tab');
-let contents = document.querySelectorAll('.tab__content');
+let tabContainers = document.querySelectorAll('.tabs');
 
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        tabs.forEach(tab => {
-          tab.classList.remove('tab_active')
+tabContainers.forEach(container => {
+    let tabs = container.querySelectorAll('.tab');
+    let contents = container.querySelectorAll('.tab__content');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(t => t.classList.remove('tab_active'));
+            contents.forEach(c => c.classList.remove('tab__content_active'));
+            tab.classList.add('tab_active');
+            contents[index].classList.add('tab__content_active');
         });
-        contents.forEach(content => {
-          content.classList.remove('tab__content_active')
-        });
-        tab.classList.add('tab_active');
-        contents[index].classList.add('tab__content_active');
     });
 });
